3d05546e83a44efcd0de27fa009f78a0bac08537
Boomika003 <149079772+Boomika003@users.noreply.github.com>
2025-07-10 10:21:37 +0530

    Time: 17 ms (48.78%), Space: 45.9 MB (22.81%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
@@ -1,40 +1,40 @@
 class Solution {
-    class Pair {
-        int u, v;
-        Pair(int u, int v) {
-            this.u = u;
-            this.v = v;
-        }
-    }
-    int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
-    int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int r = grid.length;
-        int c = grid[0].length;
-        if (grid[0][0] == 1 || grid[r - 1][c - 1] == 1) return -1;
-        int[][] dist = new int[r][c];
-        for (int[] x : dist) {
-            Arrays.fill(x, (int) 1e9);
+        int n = grid.length;
+        
+         if (grid[0][0] != 0 || grid[n - 1][n - 1] != 0) {
+            return -1;
         }
-        dist[0][0] = 0;
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(0, 0));
-        while (!q.isEmpty()) {
-            Pair p = q.poll();
-            int u = p.u;
-            int v = p.v;
-            for (int i = 0; i < 8; i++) {
-                int nr = u + dx[i];
-                int nc = v + dy[i];
-                if (!invalid(nr, nc, r, c) && grid[nr][nc] == 0 && dist[u][v] + 1 < dist[nr][nc]) {
-                    dist[nr][nc] = dist[u][v] + 1;
-                    q.add(new Pair(nr, nc));
+
+        int[][] directions = {
+            {0, -1}, {0, 1}, {1, 0}, {-1, 0}, {-1, -1}, {1, 1}, {-1, 1}, {1, -1}
+        };
+
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0, 0, 1});
+        boolean[][] vis = new boolean[n][n];
+        vis[0][0] = true;
+        
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int row = cur[0];
+            int col = cur[1];
+            int dist = cur[2];
+
+            if(row == n-1 && col == n-1)
+                return dist;
+            
+            for(int d[]:directions){
+                int newRow = row + d[0];
+                int newCol = col + d[1];
+
+                if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < n && grid[newRow][newCol] == 0 && !vis[newRow][newCol]){
+                    vis[newRow][newCol] = true;
+                    q.offer(new int[] {newRow, newCol, dist + 1});
                 }
             }
         }
-        return dist[r - 1][c - 1] != (int) 1e9 ? dist[r - 1][c - 1] + 1 : -1;
-    }
-    boolean invalid(int i, int j, int r, int c) {
-        return i < 0 || i >= r || j < 0 || j >= c;
+
+        return -1;
     }
 }
