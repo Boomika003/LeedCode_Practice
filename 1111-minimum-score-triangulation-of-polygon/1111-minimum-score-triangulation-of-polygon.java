ddaf19297a83b11f192063f9abb07580cacc6ac7
Boomika003 <149079772+Boomika003@users.noreply.github.com>
2025-09-29 14:02:35 +0530

    Time: 5 ms (38.08%), Space: 41.1 MB (79.26%) - LeetHub

--- 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.java
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.java
@@ -5,12 +5,14 @@ class Solution {
         for (int i = n - 1; i >= 0; --i) {
             for (int j = i + 1; j < n; ++j) {
                 for (int k = i + 1; k < j; ++k) {
-                    dp[i][j] = Math.min(dp[i][j] == 0 ? Integer.MAX_VALUE : dp[i][j],
-                        dp[i][k] + values[i] * values[k] * values[j] + dp[k][j]);
+                    dp[i][j] = Math.min(dp[i][j] == 0 ? Integer.MAX_VALUE : dp[i][j],dp[i][k] + values[i] * values[k] * values[j] + dp[k][j]);
                 }
             }
         }
         return dp[0][n - 1];
+      //  dp(0,1,2)=3*7*4=84;
+      //dp(0,2,3)=3*4*5=60;
+      // t=144;
 
     }
 }
